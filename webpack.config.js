10d89b164dbd6dc0d9eef6b26c178ab6a5880d2f
prasanna1211 <prasanna.krystal@gmai.com>
2016-10-26 12:59:34 +0530

    Added fonts from sass

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ var config = {
       {
         test: /\.scss$/,
         loaders: ['style', 'css', 'sass']
+      },
+      {
+        test : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader : 'file-loader'
       }
     ]
   },
