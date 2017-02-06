edf07ab065262ecfd49f0ac26b1c36d3ac3a1a4b
prasanna1211 <prasanna.krystal@gmail.com>
2017-02-06 23:10:58 +0530

    completed linting

--- src/client/app/scripts/actions/index.jsx
+++ src/client/app/scripts/actions/index.jsx
@@ -2,9 +2,8 @@
  * Action creators - To be imported in component
  * Structure each Action creator and group as per the page
  */
-
 import * as dummyActionCreators from 'scripts/actions/dummyActionCreators.jsx';
 
 export {
-	dummyActionCreators
-}
+  dummyActionCreators,
+};
