4f25c7ccfa3d7216def13d2d6df6d15549be9c39
prasanna1211 <prasanna.krystal@gmail.com>
2017-02-06 22:44:22 +0530

    es lint on store.jsx

--- src/client/app/scripts/store/index.jsx
+++ src/client/app/scripts/store/index.jsx
@@ -4,16 +4,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from 'scripts/reducers/index.jsx';
-import defaultStore from 'scripts/store/defaultStore.jsx';
 import createLogger from 'redux-logger';
 
 // Logger for redux actions and actioncreators on console
-var reduxLogger = createLogger();
+const reduxLogger = createLogger();
 
 /*
  * Single store for the entire app
  * Middleware chain thunk -> reduxLoggers.
  */
-let store = createStore(rootReducer, applyMiddleware(thunk, reduxLogger));
+const store = createStore(rootReducer, applyMiddleware(thunk, reduxLogger));
 
 export default store;
